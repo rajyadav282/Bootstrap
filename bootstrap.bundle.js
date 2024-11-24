2c7435d622eeb3d05b5c6a324c55e33c1ba043f1
Raj <94789028+rajyadav282@users.noreply.github.com>
2024-11-24 23:16:24 +0530

    Update bootstrap.bundle.js

--- bootstrap.bundle.js
+++ bootstrap.bundle.js
@@ -5,7 +5,7 @@
   */
 (function (global, factory) { 
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
+  typeof define === 'function' && drfine.amd ? define(factory) :
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.bootstrap = factory());
 })(this, (function () { 'use strict';
 
